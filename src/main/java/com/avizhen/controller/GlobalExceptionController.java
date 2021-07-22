0d14aa4e1763d83092c7ab516e3b94753e8a468e
MishaAvizhen <misha33379@mail.com>
2021-07-22 16:47:38 +0300

    add  custom employeeBirthDateException

--- src/main/java/com/avizhen/controller/GlobalExceptionController.java
+++ src/main/java/com/avizhen/controller/GlobalExceptionController.java
@@ -7,11 +7,16 @@ import com.avizhen.exception.EmployeeServiceNotFoundException;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionController {
@@ -28,6 +33,16 @@ public class GlobalExceptionController {
         ExceptionResponseDto response = buildExceptionResponse(ex, "Ooooops!!!!!");
         log.error(ex.getMessage(), ex);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<String> employeeBirthDateException(MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage(), ex);
+        return ex.getBindingResult()
+                .getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.toList());
     }
 
     private ExceptionResponseDto buildExceptionResponse(Exception ex, String errorCode) {
